b114cead4e6d29c409cc6fb44702c72755b4eacf
NgocMinh1226 <damthingocminh1226@gmail.com>
2024-03-03 17:31:00 +0700

    update file register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -54,7 +54,7 @@ border-bottom-right-radius: .3rem;
               <div class="card-body p-md-5 mx-md-4">
 
                 <div class="text-center">
-                  <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
+                  <img src="public/frontend/images/logoshop.jpg"
                     style="width: 185px;" alt="logo">
                   <h4 class="mt-1 mb-5 pb-1">Register<h4>
                 </div>
